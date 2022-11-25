23d63a7121b3afe1fca465362dd515c367421bed
CoderDoder <91969955+CoderDoder@users.noreply.github.com>
2022-11-25 18:51:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,27 @@ function snap_shot(){
 console.log("ml5 version:",ml5.version);
 
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/q_46uID7N/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model Loaded :)");
+
+}
+
+function identify(){
+    image=document.getElementById("captured_img");
+    classifier.classify(image,getResults);
+}
+function getResults(error,results){
+    if(error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+    
+   
+
+}
